00d8d02d56db5a83421e58aa71d1ef7b30a1467e
Afrose <yasser.afrose@carwale.com>
2016-02-17 18:08:02 +0530

    Added Maximum Subarray Problem solution(On2)

---
+++ Maximum Subarray Problem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+/// <summary>
+/// Maximum Subarray Problem (O^n2)
+/// </summary>
+class Solution
+{
+
+    public static void Main(String[] args)
+    {
+        int[] arr = { 0, 1, 2, -2, 3, 2 };
+        int curr_sum = 0, max_sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            curr_sum = 0;
+            for (int j = i; j < arr.Length; j++)
+            {
+               curr_sum += arr[j];
+               if (curr_sum >= max_sum)
+                   max_sum = curr_sum;
+               else break;
+               
+            }
+        }
+        Console.WriteLine(max_sum);
+        Console.ReadLine();
+
+    }
+
+
+
+}
+
+

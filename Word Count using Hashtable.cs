a51cef688bdc6467adc109038b599f5ea4cc992d
yasser_afrose <yasser.afrose@carwale.com>
2016-05-14 18:22:21 +0530

    added hashtable

---
+++ Word Count using Hashtable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static Dictionary<string, int> _wordCount = new Dictionary<string, int>();
+        static void Main(string[] args)
+        {
+            string fileName = @"C:\Users\yasser.afrose\Desktop\Algorithms and Data Structures - Part 1\Downloads\exercise-files-robert-horvick-ads-hashtable\WordCount\constitution.txt";
+            if (File.Exists(fileName))
+            {
+
+                using (FileStream contents = File.OpenRead(fileName))
+                using (StreamReader reader = new StreamReader(contents))
+                {
+                    _wordCount.Clear();
+
+                    while (!reader.EndOfStream)
+                    {
+                        AddtoHashTable(reader.ReadLine());
+                    }
+                }
+
+
+            }
+
+        }
+        public static void AddtoHashTable(string line)
+        {
+            string[] words = line.Split(" \t,.;()\"\'".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (string individual in words)
+            {
+                string tocheck = individual.ToLower();
+                int count = 0;
+                if (_wordCount.TryGetValue(tocheck, out count))  //if present
+                {
+                    _wordCount[tocheck] = count + 1;
+
+                }
+                else
+                {
+
+                    _wordCount.Add(tocheck, 1);
+                }
+            }
+        }
+    }
+}

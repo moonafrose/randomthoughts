86549f4b7ad527126f0df1c997b09085486ee355
Afrose <yasser.afrose@carwale.com>
2016-06-15 17:51:34 +0530

    Added balanced parentheses

---
+++ Balanced Parentheses.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        Integer N = Integer.parseInt(scan.nextLine());
+        
+        while(N>0)
+            {
+            String input = scan.nextLine();
+              if(isValid(input))
+                  System.out.println("YES");
+              else System.out.println("NO");
+            N--;
+            }
+        scan.close();
+            
+        }
+    public static boolean isValid(String s) {
+	HashMap<Character, Character> map = new HashMap<Character, Character>();
+	map.put('(', ')');
+	map.put('[', ']');
+	map.put('{', '}');
+ 
+	Stack<Character> stack = new Stack<Character>();
+ 
+	for (int i = 0; i < s.length(); i++) {
+		char curr = s.charAt(i);
+ 
+		if (map.keySet().contains(curr)) {
+			stack.push(curr);
+		} else if (map.values().contains(curr)) {
+			if (!stack.empty() && map.get(stack.peek()) == curr) {
+				stack.pop();
+			} else {
+				return false;
+			}
+		}
+	}
+ 
+	return stack.empty();
+}
+  
+}
